67bd511d38f1679726b4c754dc50e996bde430e0
Neil Roberts <bpeeluk@yahoo.co.uk>
2010-07-17 15:47:02 +0100

    Throw a warning if a mash-light is not parented by a MashLightBox
    MashLights don't paint anything and they are only used to affect the
    lighting within a MashLightBox so it doesn't make sense to add them to
    anything else. This patch makes it give a warning to that effect.

--- mash/mash-light.c
+++ mash/mash-light.c
@@ -25,6 +25,9 @@
 #include "mash-light.h"
 #include "mash-light-box.h"
 
+static void mash_light_parent_set (ClutterActor *self,
+                                   ClutterActor *old_parent);
+
 static void mash_light_dispose (GObject *object);
 static void mash_light_finalize (GObject *object);
 
@@ -43,9 +46,11 @@ static void
 mash_light_class_init (MashLightClass *klass)
 {
   GObjectClass *gobject_class = (GObjectClass *) klass;
+  ClutterActorClass *actor_class = (ClutterActorClass *) klass;
 
   gobject_class->dispose = mash_light_dispose;
   gobject_class->finalize = mash_light_finalize;
+  actor_class->parent_set = mash_light_parent_set;
 
   g_type_class_add_private (klass, sizeof (MashLightPrivate));
 }
@@ -54,6 +59,22 @@ static void
 mash_light_init (MashLight *self)
 {
   self->priv = MASH_LIGHT_GET_PRIVATE (self);
+}
+
+static void
+mash_light_parent_set (ClutterActor *self,
+                       ClutterActor *old_parent)
+{
+  ClutterActor *new_parent = clutter_actor_get_parent (self);
+
+  if (new_parent && !MASH_IS_LIGHT_BOX (new_parent))
+    g_warning ("A MashLight is being added to a %s. The light will only "
+               "have any effect if it is added directly to a MashLightBox",
+               G_OBJECT_TYPE_NAME (new_parent));
+
+  if (CLUTTER_ACTOR_CLASS (mash_light_parent_class)->parent_set)
+    CLUTTER_ACTOR_CLASS (mash_light_parent_class)
+      ->parent_set (self, old_parent);
 }
 
 static void
