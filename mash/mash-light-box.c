67de3c5187bb8b9982b6c6834a9898b25e517802
Neil Roberts <bpeeluk@yahoo.co.uk>
2010-07-17 17:15:51 +0100

    mash-light-box: Override the ClutterContainer add and remove methods
    This overrides the add and remove methods of ClutterContainer so that
    it could detect when the number of MashLights changes and regenerate
    the shader.

--- mash/mash-light-box.c
+++ mash/mash-light-box.c
@@ -23,10 +23,23 @@
 #include <clutter/clutter.h>
 
 #include "mash-light-box.h"
+#include "mash-light.h"
 
 static void mash_light_box_finalize (GObject *object);
 
-G_DEFINE_TYPE (MashLightBox, mash_light_box, CLUTTER_TYPE_BOX);
+static void mash_light_box_add (ClutterContainer *container,
+                                ClutterActor *actor);
+static void mash_light_box_remove (ClutterContainer *container,
+                                   ClutterActor *actor);
+
+static void mash_light_box_container_iface_init (ClutterContainerIface *iface);
+
+static ClutterContainerIface *mash_light_box_container_parent_iface;
+
+G_DEFINE_TYPE_WITH_CODE
+(MashLightBox, mash_light_box, CLUTTER_TYPE_BOX,
+ G_IMPLEMENT_INTERFACE (CLUTTER_TYPE_CONTAINER,
+                        mash_light_box_container_iface_init));
 
 #define MASH_LIGHT_BOX_GET_PRIVATE(obj) \
   (G_TYPE_INSTANCE_GET_PRIVATE ((obj), MASH_TYPE_LIGHT_BOX, \
@@ -34,7 +47,7 @@ G_DEFINE_TYPE (MashLightBox, mash_light_box, CLUTTER_TYPE_BOX);
 
 struct _MashLightBoxPrivate
 {
-  int stub;
+  CoglHandle program;
 };
 
 static void
@@ -48,6 +61,18 @@ mash_light_box_class_init (MashLightBoxClass *klass)
 }
 
 static void
+mash_light_box_container_iface_init (ClutterContainerIface *iface)
+{
+  mash_light_box_container_parent_iface = g_type_interface_peek_parent (iface);
+
+  /* We need to override the add and remove methods of the container
+     interface so that we can detect when the number of lights is
+     modified */
+  iface->add = mash_light_box_add;
+  iface->remove = mash_light_box_remove;
+}
+
+static void
 mash_light_box_init (MashLightBox *self)
 {
   self->priv = MASH_LIGHT_BOX_GET_PRIVATE (self);
@@ -57,6 +82,10 @@ static void
 mash_light_box_finalize (GObject *object)
 {
   MashLightBox *self = (MashLightBox *) object;
+  MashLightBoxPrivate *priv = self->priv;
+
+  if (priv->program)
+    cogl_handle_unref (priv->program);
 
   G_OBJECT_CLASS (mash_light_box_parent_class)->finalize (object);
 }
@@ -67,4 +96,41 @@ mash_light_box_new (void)
   ClutterActor *self = g_object_new (MASH_TYPE_LIGHT_BOX, NULL);
 
   return self;
+}
+
+static void
+mash_light_box_check_light_changed (MashLightBox *light_box,
+                                    ClutterActor *actor)
+{
+  MashLightBoxPrivate *priv = light_box->priv;
+
+  /* If we've added or removed a light then we need to regenerate the
+     shader */
+  if (MASH_IS_LIGHT (actor) && priv->program != COGL_INVALID_HANDLE)
+    {
+      cogl_handle_unref (priv->program);
+      priv->program = COGL_INVALID_HANDLE;
+    }
+}
+
+static void
+mash_light_box_add (ClutterContainer *container,
+                    ClutterActor *actor)
+{
+  MashLightBox *light_box = MASH_LIGHT_BOX (container);
+
+  mash_light_box_container_parent_iface->add (container, actor);
+
+  mash_light_box_check_light_changed (light_box, actor);
+}
+
+static void
+mash_light_box_remove (ClutterContainer *container,
+                       ClutterActor *actor)
+{
+  MashLightBox *light_box = MASH_LIGHT_BOX (container);
+
+  mash_light_box_container_parent_iface->remove (container, actor);
+
+  mash_light_box_check_light_changed (light_box, actor);
 }
